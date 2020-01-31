2b6ba8b3b1ed5076972940cae7b5452a23c12dcc
fangmin.yan <fangmin.yan@lvyuetravel.com>
2020-01-31 20:44:02 +0800

    优化

--- src/api/index/other/index.js
+++ src/api/index/other/index.js
@@ -4,12 +4,16 @@
  */
 import React from 'react';
 import ajax from '@/assets/utils/ajax'
+import { environmentSwitch } from '@/assets/env.js'
 import request from './file'
+
 const domains = React.$domains
+const { NODE_ENV } = process.env
+const ENV = environmentSwitch(NODE_ENV) // 获取环境
 
 export const reqMovie = (id) => ajax({
     url: `${request.FETCH_MOVIE_BY_ID}/${id}`,
     data: {},
     type: 'GET',
-    baseURL: domains.baseUrl
+    baseURL: domains[ENV].baseUrl
 })
