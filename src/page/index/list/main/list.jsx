761ee2f902befca10c627933e4b5c428907c6e7a
yanfangmin <yanfangmin@okay.cn>
2020-07-02 21:47:47 +0800

    update

--- src/page/index/list/main/list.jsx
+++ src/page/index/list/main/list.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import { hot } from 'react-hot-loader'
 import intl from 'react-intl-universal';
 import moment from 'moment'
+import { Table, Tree, Checkbox } from '@/mayflyComponent'
+import '@/mayflyComponent/component/table/style/css.js';
 import './list.css'
 import {
     TransitionGroup, CSSTransition
@@ -22,6 +24,36 @@ import { getMockDataGood, changeGood, changeLoading } from '@/redux/index/list/a
 import { Loading, Content } from '@/component/index'
 
 const total = 42
+const datacolumn = [
+    {
+        title: '一',
+        prop: '一',
+        width: '10%'
+    }, {
+        title: '二',
+        prop: '二',
+        width: '10%'
+    }, {
+        title: '三',
+        prop: '三',
+        width: '10%'
+    }, {
+        title: '四',
+        width: '10%',
+        prop: '四'
+    }, {
+        title: '五',
+        prop: '五',
+        width: '10%'
+    }, {
+        title: '六',
+        width: '10%',
+        prop: '六'
+    }, {
+        title: '日',
+        width: '10%',
+        prop: '日'
+    }]
 class List extends React.Component {
     constructor(props) {
         super(props);
@@ -49,7 +81,8 @@ class List extends React.Component {
                 5: '五',
                 6: '六',
                 0: '日'
-            }
+            },
+            data: []
         }
     }
 
@@ -62,37 +95,37 @@ class List extends React.Component {
     }
 
     momentHandle = () => {
-        console.log(moment().add(-10, 'd').format('DD'), moment().format('D'), moment().day(), moment().startOf('month').day(), '??????????')
+        // console.log(moment().add(-10, 'd').format('DD'), moment().format('D'), moment().day(), moment().startOf('month').day(), '??????????')
         const { dayToDate } = this.state
         const day = moment().startOf('month').day()
+        const dayFirst = moment().startOf('month').format('D')
         const date = moment().format('D')
-        const num = moment().daysInMonth()
+        const num = moment().daysInMonth() - date
         const beforeArr = []
         const beforeTodayArr = []
         const afterTodayArr = []
         const makeUpArr = []
         for (let i = -1; i > -day; i--) {
-            const day = moment().add(i, 'd').day()
-            const date = moment().add(i, 'd').format('D')
+            const day = moment().startOf('month').add(i, 'd').day()
+            const date = moment().startOf('month').add(i, 'd').format('D')
             let obj = {}
-            obj.title = dayToDate[day]
-            obj.dataIndex = date
-            beforeArr.push(obj)
+            // obj.title = dayToDate[day]
+            obj[dayToDate[day]] = date
+            beforeArr.unshift(obj)
         }
         for (let i = 0; i > -date; i--) {
             const day = moment().add(i, 'd').day()
             const date = moment().add(i, 'd').format('D')
             let obj = {}
-            obj.title = dayToDate[day]
-            obj.dataIndex = date
-            beforeTodayArr.push(obj)
+            // if (dayFirst == date) continue
+            obj[dayToDate[day]] = date
+            beforeTodayArr.unshift(obj)
         }
-        for (let i = 1; i < num; i++) {
+        for (let i = 1; i <= num; i++) {
             const day = moment().add(i, 'd').day()
             const date = moment().add(i, 'd').format('D')
             let obj = {}
-            obj.title = dayToDate[day]
-            obj.dataIndex = date
+            obj[dayToDate[day]] = date
             afterTodayArr.push(obj)
         }
         const len = [...beforeArr, ...beforeTodayArr, ...afterTodayArr].length
@@ -101,12 +134,30 @@ class List extends React.Component {
             const day = moment().endOf('month').add(i, 'd').day()
             const date = moment().endOf('month').add(i, 'd').format('D')
             let obj = {}
-            obj.title = dayToDate[day]
-            obj.dataIndex = date
+            obj[dayToDate[day]] = date
             makeUpArr.push(obj)
         }
-        console.log(beforeArr, len, '"""""""')
-        console.log(beforeTodayArr)
+        const arrs = [...beforeArr, ...beforeTodayArr, ...afterTodayArr, ...makeUpArr]
+        console.log(arrs)
+        const dateCon = []
+        for (let i = 0; i < arrs.length; i += 7) {
+            const arr = arrs.slice(i, i + 7)
+            const obj = {}
+            arr.map((item, index) => {
+                for (let key in item) {
+                    obj[key] = item[key]
+                }
+            })
+            console.log(obj)
+            dateCon.push(obj)
+        }
+        this.setState({
+            data: dateCon
+        }, () => {
+            // console.log(this.state.data, '>?????????')
+        })
+        // console.log(beforeArr, len, '"""""""')
+        // console.log(beforeTodayArr)
         console.log(afterTodayArr)
         console.log(makeUpArr)
     }
@@ -175,35 +226,42 @@ class List extends React.Component {
     };
 
     render() {
-        const { goods, loading } = this.props
-        const len = goods.length
+        // const { goods, loading } = this.props
+        // const len = goods.length
 
-        switch (len) {
-            case 1:
-                return (
-                    <div className="app-container">
-                        <Content
-                            lang = {this.state.lang}
-                            SUPPOER_LOCALES = {this.state.SUPPOER_LOCALES}
-                            onSelectLocale = {this.onSelectLocale}/>
-                        {/* <div onClick={this.pushGoods}>改变goods</div>
-                        <div onClicks={this.changeLoadings}>改变loading</div> */}
-                        {/* <Loading show={loading}/> */}
-                        {/* {goods && goods.length ? goods.map((good,index) => {
-                                return <p>12344</p>
-                        }) : null} */}
+        return (
+            <Table
+                dataconf={datacolumn}
+                dataset={this.state.data}
+                loading={false}
+                isMaintenance={false} />
+        )
+        // switch (len) {
+        //     case 1:
+        //         return (
+        //             <div className="app-container">
+        //                 <Content
+        //                     lang = {this.state.lang}
+        //                     SUPPOER_LOCALES = {this.state.SUPPOER_LOCALES}
+        //                     onSelectLocale = {this.onSelectLocale}/>
+        //                 {/* <div onClick={this.pushGoods}>改变goods</div>
+        //                 <div onClicks={this.changeLoadings}>改变loading</div> */}
+        //                 {/* <Loading show={loading}/> */}
+        //                 {/* {goods && goods.length ? goods.map((good,index) => {
+        //                         return <p>12344</p>
+        //                 }) : null} */}
 
-                    </div>
-                )
-                break;
-            case 2:
-                return (
-                    <div>123456789</div>
-                )
-                break;
-            default:
-                return (<p>0</p>)
-        }
+        //             </div>
+        //         )
+        //         break;
+        //     case 2:
+        //         return (
+        //             <div>123456789</div>
+        //         )
+        //         break;
+        //     default:
+        //         return (<p>0</p>)
+        // }
 
     }
 }
